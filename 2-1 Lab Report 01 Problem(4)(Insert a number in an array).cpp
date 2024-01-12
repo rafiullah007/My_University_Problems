a0486d523ce0ce1063538e310aee4fd8f8937aa3
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-12 21:48:33 +0600

    Update 2-1 Lab Report 01 Problem(4)(Insert a number in an array).cpp

--- 2-1 Lab Report 01 Problem(4)(Insert a number in an array).cpp
+++ 2-1 Lab Report 01 Problem(4)(Insert a number in an array).cpp
@@ -8,6 +8,16 @@
 #include <iostream>
 using namespace std;
 
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
 int main()
  {
     int s, p;
