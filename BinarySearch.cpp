b5dc76c820dfd6a20fe46f1347393306ded4e891
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-03-11 21:01:16 +0600

    Add files via upload

---
+++ BinarySearch.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int a[], int l , int r , int t)
+{
+    while(l<=r)
+    {
+        int m = l+(r-l)/2;
+        if(a[m]==t) return m;
+        else if(a[m]<t) l = m+1;
+        else r = m-1;
+    }
+    return -1;
+}
+void input(int a[], int n)
+{
+    for(int i = 0 ; i<n; i++)
+    {
+        cin>>a[i];
+    }
+}
+
+int main(void)
+{
+    int n;
+    cin>>n;
+    int a [n];
+    cout<<"Enter the elements of your arrays : " ;
+    input(a,n);
+    int res = BinarySearch( a, 0 , 5-1 , 8); /// target = 8
+    (res==-1)
+    ? cout<<"Element is not found"<<endl
+    : cout<<"Element Found at index : " <<res<<endl;
+}
+
