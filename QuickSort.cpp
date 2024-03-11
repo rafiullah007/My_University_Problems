94c5243226677cfdbdfdd856e314728d65b30c60
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-03-11 21:07:17 +0600

    Add files via upload

---
+++ QuickSort.cpp
@@ -0,0 +1,49 @@
+/// QuickSort.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int part(int a[], int l ,int h)
+{
+    int pvt = a[l];
+    int i = l+1;
+    int j = h;
+    while(i<=j)
+    {
+        while (a[i]<pvt && i <=h) i++;
+        while (a[j]>pvt && j>=l) j--;
+        if(i<j)
+        {
+            swap(a[i],a[j]);  i++; j--;
+        }
+    }
+    swap(a[j],a[l]);
+    return j;
+}
+
+void QS (int a[], int l, int h)
+{
+    if(l<h)
+    {
+        int pi = part(a,l,h);
+        QS(a,l,pi-1);
+        QS(a,pi+1,h);
+    }
+
+
+}
+
+
+int main()
+{
+    int a[] = {13,4,5,6,4,3,1,2,3,4,324,3,2,2,1,7};
+    int n =  sizeof(a)/ sizeof(a[0]);
+    QS(a,0,n-1);
+    for(int i = 0 ; i<n ; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
