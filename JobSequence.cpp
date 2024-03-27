8ea55b3687375a62424c08e160dd2148ce1864bc
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-03-27 21:20:05 +0600

    Update JobSequence.cpp

--- JobSequence.cpp
+++ JobSequence.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+
 int main()
 {
     /// T =  Question array
