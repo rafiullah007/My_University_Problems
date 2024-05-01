10a9248c3a5063bb46c6390a62402189f13fe44e
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-05-01 16:09:12 +0600

    Update Longest Common Subsequence (LCS).cpp

--- Longest Common Subsequence (LCS).cpp
+++ Longest Common Subsequence (LCS).cpp
@@ -12,15 +12,15 @@ using namespace std;
     */
 
 
-int LCS(string a, string b, int m, int n)
+int LCS(string s1, string s2, int m, int n)
 {
 	if (m == 0 || n == 0)
 		return 0;
-	if (a[m - 1] == b[n - 1])
-		return 1 + LCS(a, b, m - 1, n - 1);
+	if (s1[m - 1] == s2[n - 1])
+		return 1 + LCS(s1, s2, m - 1, n - 1);
 	else
-		return max(LCS(a, b, m, n - 1),
-				LCS(a, b, m - 1, n));
+		return max(LCS(s1, s2, m, n - 1),
+				LCS(s1, s2, m - 1, n));
 }
 
 
