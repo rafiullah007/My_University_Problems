d135e67be924d566fdc95af6d8d9e04a22718b56
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-05-01 15:57:50 +0600

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,31 +1,40 @@
-#include <stdio.h>
-/* Minnatul Jannat Mimi
-   2212866643
-   */
+#include <iostream>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+
 void listNumbersDesc(int s, int e) {
     if (s > e) {
-        printf("Error!!!\n");
+        cout << "Error!!!" <<endl;
         return;
     }
 
-    printf("Numbers in descending order from %d to %d:\n", s, e);
+    cout << "Numbers in descending order from " << s << " to " << e << ":" <<endl;
     for (int i = e; i >= s; i--) {
-        printf("%d ", i);
+        cout << i << " ";
     }
-    printf("\n");
+    cout << std::endl;
 }
 
 int main() {
     int s, e;
 
-    printf("Enter the starting number: ");
-    scanf("%d", &s);
+    cout << "Enter the starting number: ";
+    cin >> s;
 
-    printf("Enter the ending number: ");
-    scanf("%d", &e);
+    cout << "Enter the ending number: ";
+    cin >> e;
 
     listNumbersDesc(s, e);
 
     return 0;
 }
-
